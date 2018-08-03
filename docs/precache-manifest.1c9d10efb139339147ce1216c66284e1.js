75f4ef4cc692f40afd6f69df17a7166929b19b6e
juany-ramirez <juany.ramrz@unitec.edu>
2018-08-03 09:31:40 -0600

    More route changes

--- docs/precache-manifest.1c9d10efb139339147ce1216c66284e1.js
+++ docs/precache-manifest.1c9d10efb139339147ce1216c66284e1.js
@@ -1,42 +1,42 @@
 self.__precacheManifest = [
   {
     "revision": "c260196379be1d7f052a",
-    "url": "/js/chunk-vendors.c2601963.js"
+    "url": "/Dicel/js/chunk-vendors.c2601963.js"
   },
   {
     "revision": "22c690b2dce1707ba307",
-    "url": "/js/app.22c690b2.js"
+    "url": "/Dicel/js/app.22c690b2.js"
   },
   {
     "revision": "a2104bce0b07069101fdbc4d774e39ef",
-    "url": "/index.html"
+    "url": "/Dicel/index.html"
   },
   {
     "revision": "d6879d70eb326f7a11784c7d95eed940",
-    "url": "/img/dusk-electric.d6879d70.jpg"
+    "url": "/Dicel/img/dusk-electric.d6879d70.jpg"
   },
   {
     "revision": "24d2c73e7984d7177623405add2f642a",
-    "url": "/img/dicelPro-02.24d2c73e.png"
+    "url": "/Dicel/img/dicelPro-02.24d2c73e.png"
   },
   {
     "revision": "cc5e79f7d1c8d38fb64fb396e2c46956",
-    "url": "/img/dicel-original.cc5e79f7.png"
+    "url": "/Dicel/img/dicel-original.cc5e79f7.png"
   },
   {
     "revision": "08053a4057aec4d8103b3b5c912eac59",
-    "url": "/img/beach-clouds.08053a40.jpg"
+    "url": "/Dicel/img/beach-clouds.08053a40.jpg"
   },
   {
     "revision": "e900401b01e597ca5252e4dc60877f7e",
-    "url": "/img/architecture-building.e900401b.jpg"
+    "url": "/Dicel/img/architecture-building.e900401b.jpg"
   },
   {
     "revision": "c260196379be1d7f052a",
-    "url": "/css/chunk-vendors.e1b559b7.css"
+    "url": "/Dicel/css/chunk-vendors.e1b559b7.css"
   },
   {
     "revision": "22c690b2dce1707ba307",
-    "url": "/css/app.309776c0.css"
+    "url": "/Dicel/css/app.309776c0.css"
   }
 ];
